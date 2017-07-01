9ed768e6ee5ebdb2db30d25a7dfb87759170f285
unknown <loic2845@gmail.com>
2017-07-01 19:19:21 +0200

    commit ok

--- server/server.js
+++ server/server.js
@@ -8,6 +8,20 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+	res.header("Access-Control-Allow-Headers", "content-type, X-Requested-With");
+	res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
 var server = app.listen(process.env.PORT || 3000, function () {
 	var port = server.address().port;
 	console.log("App now running on port", port);
